9764df931df020695b3e7137f2fba2f674deecda
alex.dana@epitech.eu <alex.dana@epitech.eu>
2020-01-06 12:21:45 +0100

    update yarn

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       repo: "git@github.com:misteralex95/tic-tac-toc.git",
       path: "/home/deploy/tic-tac-toc",
       "post-deploy":
-        ". ~/.bashrc && npm install && pm2 reload ecosystem.config.js --env production && npm start",
+        ". ~/.bashrc && yarn install && pm2 reload ecosystem.config.js --env production && yarn start",
       env: {
         NODE_ENV: "production"
       }
