7a3d80dd8c76cb6314656355500689aff17de329
alex.dana@epitech.eu <alex.dana@epitech.eu>
2020-01-07 21:08:55 +0100

    test serv

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,17 @@
 module.exports = {
-  apps: [
-    {
-      name: "tic-tac-toc",
-      script: "npx",
-      interpreter: "none",
-      args: "serve build -s",
-      env_production: {
-        NODE_ENV: "production"
-      }
-    }
-  ],
+  apps: [],
   deploy: {
     production: {
       user: "deploy",
       host: "alexdana.me",
       ref: "origin/master",
       repo: "git@github.com:misteralex95/tic-tac-toc.git",
-      path: "/home/deploy/tic-tac-toc"
+      path: "/home/deploy/tic-tac-toc",
+      "post-deploy":
+        ". ~/.bashrc && npm install && pm2 reload ecosystem.config.js --env production && npm run build",
+      env: {
+        NODE_ENV: "production"
+      }
     }
   }
 };
