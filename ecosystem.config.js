e8dfe22bf8bf4289ab067554b4b96346ac556fcd
alex.dana@epitech.eu <alex.dana@epitech.eu>
2020-01-07 18:55:20 +0100

    update npm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       repo: "git@github.com:misteralex95/tic-tac-toc.git",
       path: "/home/deploy/tic-tac-toc",
       "post-deploy":
-        ". ~/.bashrc && npm install && pm2 reload ecosystem.config.js --env production && npm start",
+        ". ~/.bashrc && npm install && pm2 reload ecosystem.config.js --env production && npm run build",
       env: {
         NODE_ENV: "production"
       }
