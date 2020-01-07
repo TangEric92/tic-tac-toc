4baf290ba8519eaaf7a0df6b274bf0d3d3a37dd9
alex.dana@epitech.eu <alex.dana@epitech.eu>
2020-01-07 21:04:43 +0100

    test serv

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,12 +16,7 @@ module.exports = {
       host: "alexdana.me",
       ref: "origin/master",
       repo: "git@github.com:misteralex95/tic-tac-toc.git",
-      path: "/home/deploy/tic-tac-toc",
-      "post-deploy":
-        ". ~/.bashrc && npm install && pm2 reload ecosystem.config.js --env production && npm run build",
-      env: {
-        NODE_ENV: "production"
-      }
+      path: "/home/deploy/tic-tac-toc"
     }
   }
 };
